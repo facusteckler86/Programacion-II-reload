62c0916a87057768d8072a68e8abb7710a791027
facundomsteckler@gmail.com <facundomsteckler@gmail.com>
2026-08-09 17:27:41 -0300

    arreglo errores de Program

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Bienvenido Facundo");
+
+Persona Facundo = new Persona();
+Facundo.nombre = "Facundo";
+Facundo.apellido = "Steckler";
+
+Facundo.Presentarse();
+
+class Persona
+{
+    public string nombre = "";
+    public string apellido = "";
+
+    public void Presentarse()
+    {
+        Console.WriteLine(this.apellido + "" + this.nombre);
+    }
+    
+}
--- src/ConsoleApp/obj/Debug/net10.0/ConsoleApp.AssemblyInfo.cs
+++ src/ConsoleApp/obj/Debug/net10.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70507ccb97b39345b987c464fd3756ff38621444")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
